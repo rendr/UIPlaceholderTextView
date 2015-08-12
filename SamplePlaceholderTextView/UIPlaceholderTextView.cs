b325015605e68d255213ff053d992c6f2defd1db
David Ortinau <dave@rendr.io>
2015-08-11 22:28:30 -0500

    sample project with component. can tap outside to see the placeholder replaced if empty

---
+++ SamplePlaceholderTextView/UIPlaceholderTextView.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UIKit;
+using Foundation;
+
+
+namespace Rendr.UI
+{
+	[Register ("UIPlaceholderTextView")]
+	public class UIPlaceholderTextView : UITextView
+	{
+		private string placeHolderText;
+		public string PlaceHolderText {
+			get {
+				return placeHolderText;
+			}
+			set {
+				placeHolderText = value;
+				if(string.IsNullOrEmpty(Text)){
+					Text = placeHolderText;
+					TextColor = UIColor.LightGray;
+				}
+			}
+		}
+
+		void Init ()
+		{
+			this.Delegate = new UIPlaceholderTextViewDelegate ();
+		}
+
+		public UIPlaceholderTextView ()
+		{
+			Init ();
+		}
+
+		public UIPlaceholderTextView (IntPtr handle) : base (handle)
+		{
+			Init();
+		}
+
+		public override void AwakeFromNib ()
+		{
+			base.AwakeFromNib ();
+
+			Init();
+		}
+	}
+
+	public class UIPlaceholderTextViewDelegate : UITextViewDelegate
+	{
+		public override bool ShouldBeginEditing (UITextView textView)
+		{
+			textView.TextColor = UIColor.Black;
+
+			if(textView.Text == ((UIPlaceholderTextView)textView).PlaceHolderText) {
+				textView.Text = "";
+			}
+
+			return true;
+		}
+
+		public override void EditingEnded (UITextView textView)
+		{
+			if(textView.Text == "") {
+				textView.Text = ((UIPlaceholderTextView)textView).PlaceHolderText;
+				textView.TextColor = UIColor.LightGray;
+			}
+		}
+	}
+}
+
---
+++ SamplePlaceholderTextView/ViewController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using UIKit;
+
+namespace SamplePlaceholderTextView
+{
+	public partial class ViewController : UIViewController
+	{
+		private UITapGestureRecognizer _tapGestureRecognizer;
+
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			MyTextView.PlaceHolderText = "Write about your feelings here...";
+
+			InitTapOutside();
+		}
+
+		void InitTapOutside ()
+		{
+			_tapGestureRecognizer = new UITapGestureRecognizer (OnTapOutside);
+			this.View.AddGestureRecognizer(_tapGestureRecognizer);
+		}
+
+		void OnTapOutside(UIGestureRecognizer recognizer){
+			MyTextView.ResignFirstResponder();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
